eb26284b04b93e41695c47547bcc9f4c06399bf9
Keishi Asai <isheik808@gmail.com>
2017-08-27 23:59:08 -0700

    Update import react-router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Route, Switch } from "react-router";
 
 class App extends React.Component {
     constructor(props) { 
