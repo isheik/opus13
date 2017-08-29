36d4e2a3225755fc3c190fed4aeea6f70c6c37c8
Keishi Asai <isheik808@gmail.com>
2017-08-28 23:27:29 -0700

    Fix now app render the main component initially

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Redirect, Switch, Route } from 'react-router-dom';
 import Home from "./Home";
 import Test from "./Test";
 
 const Main = () => (
     <main>
         <Switch>
+            {window.location.pathname.includes('index.html') && <Redirect to="/" />}
             <Route exact path='/' component={Home}/>
             <Route path='/test' component={Test}/>
         </Switch>
