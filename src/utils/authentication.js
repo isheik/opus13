06a262ed195cf4ce50387305ede62f426d6335cd
Keishi Asai <isheik808@gmail.com>
2018-01-27 18:03:03 -0800

    Add request parameters

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -1,11 +1,32 @@
-import twit from 'node-twitter-api';
+// import twitter from 'node-twitter-api';
 
-export default class Authentication {
+
+class Authentication {
   static APP_KEY = '9kyGvxw2hN6RUwQ2MZ9h3WBtV';
-  static APP_SCRT_KEY = 'qfEF4Z89AN9WBn80dO91WpBowmUs8AmMLPTTqhA4s14Fyl1AGz';
+  static APP_SECRET_KEY = 'qfEF4Z89AN9WBn80dO91WpBowmUs8AmMLPTTqhA4s14Fyl1AGz';
+  static CALLBACK_URL = 'https://example.com';
+  static REQUEST_URL = 'https://api.twitter.com/oauth/request_token';
+  static REQUEST_METHOD = 'POST';
+  static ACCESS_TOKEN_SECRET = '';
 
   test() {
     this.retstr = 'test';
     return 'testa';
   }
+
+  generateSignature() {
+    // Generate Key
+    let signatureKey = `${encodeURI(this.APP_SECRET_KEY)} & ${encodeURI(this.ACCESS_TOKEN_SECRET)}`;
+
+    // Generate Data
+    let params = {
+      oauth_callback: this.CALLBACK_URL,
+      oauth_consumer_key: this.APP_KEY,
+      oauth_signature_method: 'HMAC-SHA1',
+      oauth_timestamp: Date.now(),
+      // oauth_nonce:
+    };
+  }
 }
+
+export default Authentication;
