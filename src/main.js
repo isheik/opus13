cf9063d8472d90e7cdfafef9eacdca0c2b963f50
Keishi Asai <isheik808@gmail.com>
2018-03-09 23:05:17 -0800

    Fix main.js format

--- src/main.js
+++ src/main.js
@@ -13,28 +13,26 @@ import { app, BrowserWindow, ipcMain } from 'electron';
 // var BrowserWindow = electron.BrowserWindow;
 // var BrowserWindow = require('electron').remote.BrowserWindow;
 // const Authentication = require('./src/utils/authentication');
-import path from 'path';
+// import path from 'path';
 import Authentication from './utils/authentication';
-
-let base = path.resolve(__dirname);
-console.log(base);
 
 // console.log(BrowserWindow);
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 global.mainWindow = null;
 
-app.on('window-all-closed', function () {
-  if (process.platform != 'darwin')
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
+  }
 });
 
-app.on('ready', function () {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+app.on('ready', () => {
+  global.mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  global.mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  mainWindow.on('closed', function () {
-    mainWindow = null;
+  global.mainWindow.on('closed', () => {
+    global.mainWindow = null;
   });
 });
 
