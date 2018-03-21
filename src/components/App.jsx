97bf57cdf5e03c69e23c3d348b6c2153cc8d31b7
Keishi Asai <isheik808@gmail.com>
2018-03-20 23:48:18 -0700

    Delete unnecessary comment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,38 +2,6 @@ import React from 'react';
 import SideMenu from './SideMenu';
 import Main from './Main';
 
-// class App extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = { message: "something" };
-//     }
-
-//     onChange(e) {
-//         this.setState( {message: e.target.value} );
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <div>
-//                     <input type="text" onChange = { this.onChange.bind(this) } />
-//                     <p>{ this.state.message }</p>
-//                 </div>
-//                 <div>
-//                     <h1>Simple app</h1>
-//                     <ul className="header">
-//                         <li>Home</li>
-//                         <li>Stuff</li>
-//                         <li>Contact</li>
-//                     </ul>
-//                 </div>
-//                 <div className="content">
-//                     {this.props.children}
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
 // probaably have to bind this, u changed store.js too, so work on it too.
 // If you use this.props inside App module, you have to change this expression
 // from anonymous function to class or using React.CreaateClass
