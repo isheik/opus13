cda98ce8fc14ce6bd4347f54087ceefe97f2ebea
Keishi Asai <isheik808@gmail.com>
2018-03-22 23:36:33 -0700

    Delete unncessary codes

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,28 +1,28 @@
 const Actions = {
-  addTodo(value) {
-    return {
-      type: 'ADDTASK',
-      value,
-    };
-  },
-  fixTodo(value) {
-    return {
-      type: 'FIXTASK',
-      value,
-    };
-  },
-  updTodo(value) {
-    return {
-      type: 'UPDTASK',
-      value,
-    };
-  },
-  delTodo(value) {
-    return {
-      type: 'DELTASK',
-      value,
-    };
-  },
+  //   addTodo(value) {
+  //     return {
+  //       type: 'ADDTASK',
+  //       value,
+  //     };
+  //   },
+  //   fixTodo(value) {
+  //     return {
+  //       type: 'FIXTASK',
+  //       value,
+  //     };
+  //   },
+  //   updTodo(value) {
+  //     return {
+  //       type: 'UPDTASK',
+  //       value,
+  //     };
+  //   },
+  //   delTodo(value) {
+  //     return {
+  //       type: 'DELTASK',
+  //       value,
+  //     };
+  //   },
 };
 
 export default Actions;
