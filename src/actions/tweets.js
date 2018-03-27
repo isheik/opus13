22748fbcdd26bf437c9b299aededa43be7f89bcc
Keishi Asai <isheik808@gmail.com>
2018-03-26 23:41:15 -0700

    Add action

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,15 @@
-const loadHome = account => (
+export const loadHome = account => (
   {
     type: 'LOAD_HOME',
   }
 );
 
+export const addTweetToTab = (tweets, tab) => (
+  {
+    type: 'ADD_TWEET_TO_TAB',
+    tweets,
+    tab,
+  }
+);
 
 export default loadHome;
