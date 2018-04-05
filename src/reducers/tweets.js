c0e2f79feee2c7c39ce6c0fe6370c7ca32c491fc
Keishi Asai <isheik808@gmail.com>
2018-04-05 00:10:53 -0700

    Successfully displayed tweet data via redux! 🚀

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -2,13 +2,14 @@
 export const tweets = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TWEET_TO_TAB':
-      return [
-        ...state,
-        {
-          tweets: action.tweets,
-          tab: action.tab,
-        }
-      ];
+      // return [
+      //   ...state,
+      //   {
+      //     tweets: action.tweets,
+      //     tab: action.tab,
+      //   }
+      // ];
+      return action.tweets;
     default:
       return state;
   }
