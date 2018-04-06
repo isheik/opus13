3562a9cd5fb54837008bc1e6bf4c019efdfa29b8
Keishi Asai <isheik808@gmail.com>
2018-04-05 23:53:30 -0700

    Add home

---
+++ src/containers/Home.jsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import Home from '../components/Home';
+
+const mapStateToProps = state => (
+  {
+
+  }
+);
+
+const mapDispatchToProps = props => (
+  {
+
+  }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
