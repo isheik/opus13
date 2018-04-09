59670d5b07f69d0b973c34a282a49742df77cb6e
Keishi Asai <isheik808@gmail.com>
2018-04-08 22:32:08 -0700

    Test stopPropagation

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -6,7 +6,7 @@ const SideMenu = () => (
     <nav className="app-global-nav">
       <ul className="app-nav-menu">
         <li className="app-nav-items">
-          <Link to="/" onClick={console.log('test')}>Home</Link>
+          <Link to="/" onClick={(event) => { event.stopPropagation(); console.log('test'); }}>Home</Link>
         </li>
         <li className="app-nav-items">
           <Link to="/test">Test</Link>
