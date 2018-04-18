a391c9b0ac77ceca36fefef0b73f7fc2367f2cb9
Keishi Asai <isheik808@gmail.com>
2018-04-18 00:41:00 -0700

    Need to solve tweet key dup

--- src/components/TweetList.jsx
+++ src/components/TweetList.jsx
@@ -3,10 +3,10 @@ import TweetItem from './TweetItem';
 
 const TweetList = props => (
   <div>
+    {props.tweets.map(tweetObj => <TweetItem {...props} key={tweetObj.tweet.id_str} tweet={tweetObj.tweet} />)}
     {console.log(props.tweets)}
   </div>
 );
-// {props.tweets.map(tweet => <TweetItem {...props} key={tweet.id_str} tweet={tweet} />)}
 
 // {props.accounts.map(account => console.log(account))}
 // {console.log(props.tweets)}
