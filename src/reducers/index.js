754a1abff3616505ff3e96bf78d3f0399d6b2085
Keishi Asai <isheik808@gmail.com>
2018-04-24 22:29:31 -0700

    Add new reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,11 @@ import { routerReducer } from 'react-router-redux';
 
 import tweets from './tweets';
 import accounts from './accounts';
+import activeAccount from './activeAccount';
 
 // combine reducers. but each reducers have their own states.
 const rootReducer = combineReducers({
-  tweets, accounts, routing: routerReducer,
+  tweets, accounts, activeAccount, routing: routerReducer,
 });
 
 export default rootReducer;
