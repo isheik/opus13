e21f820a580326aab8e66ec52863cfd96f82e1ba
Keishi Asai <isheik808@gmail.com>
2018-04-24 23:03:39 -0700

    Tuckle account problems...

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,10 @@ import TweetListContainer from '../containers/TweetListContainer';
 
 const Home = props => (
   <div>
-    {console.log(props.activeAccountIndex)}
-    <TweetListContainer {...props} account={props.accounts['0']} tab="home" />
+    {console.log(props.accounts)}
+    <TweetListContainer {...props} account={(props.accounts && props.accounts[props.activeAccountIndex])} tab="home" />
   </div>
 );
 // props.activeAccountIndex
+// {console.log(props.activeAccountIndex)}
 export default Home;
--- src/containers/TweetListContainer.jsx
+++ src/containers/TweetListContainer.jsx
@@ -2,9 +2,11 @@ import { connect } from 'react-redux';
 import TweetList from '../components/TweetList';
 
 const mapStateToProps = (state, props) => {
-  let tweetData = state.tweets['2195738078'];
+  console.log(props.account);
+  let tweetData = (state.tweets && state.tweets[(props.account && props.account['user_id'])]) || null;
   // let tweetsa = (tweetData && tweetData[props.tab]) || [];
   let tweetsa = (tweetData && tweetData[props.tab]) || [];
+  console.log(props.tweetsa);
 
   return {
     // tweets: (tweetData[props.tab] || []),
