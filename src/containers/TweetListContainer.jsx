ed1fe8ee4ca4a7010235cc71929dd307d1e9a24e
Keishi Asai <isheik808@gmail.com>
2018-04-24 23:19:11 -0700

    Fix var names

--- src/containers/TweetListContainer.jsx
+++ src/containers/TweetListContainer.jsx
@@ -2,16 +2,14 @@ import { connect } from 'react-redux';
 import TweetList from '../components/TweetList';
 
 const mapStateToProps = (state, props) => {
-  console.log(props.account);
   let tweetData = (state.tweets && state.tweets[(props.account && props.account.user_id)]) || null;
   // let tweetData = state.tweets['2195738078'];
   // let tweetsa = (tweetData && tweetData[props.tab]) || [];
-  let tweetsa = (tweetData && tweetData[props.tab]) || [];
-  console.log(props.tweetsa);
+  let tweets = (tweetData && tweetData[props.tab]) || [];
 
   return {
     // tweets: (tweetData[props.tab] || []),
-    tweets: tweetsa,
+    tweets: tweets,
   };
 };
 // "2195738078"
