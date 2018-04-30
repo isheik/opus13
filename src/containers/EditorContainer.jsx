52074c86c08d5bb51c350289cb4968fe3fe064f1
Keishi Asai <isheik808@gmail.com>
2018-04-29 22:55:40 -0700

    Add EditorContainer

---
+++ src/containers/EditorContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Editor from '../components/Editor';
+
+const mapStateToProps = () => (
+  {}
+);
+
+const mapDispatchToProps = () => (
+  {}
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Editor);
