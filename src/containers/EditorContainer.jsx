8240b0dd0d97fb0d6d6b891a6e83639f7739f772
Keishi Asai <isheik808@gmail.com>
2018-04-30 22:34:49 -0700

    Need to fix Japanese Input Enter event handling

--- src/containers/EditorContainer.jsx
+++ src/containers/EditorContainer.jsx
@@ -38,6 +38,8 @@ const mapDispatchToProps = (dispatch, props) => (
           //     // dispatch(actions.addTweetToTab(tweets, 'home'));
           //   }
           // });
+        } else {
+          console.log(error);
         }
       });
     },
