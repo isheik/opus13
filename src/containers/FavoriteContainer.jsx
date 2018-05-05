6e042e04f1df75cd963a5faa25105e0da4a3c9ec
Keishi Asai <isheik808@gmail.com>
2018-05-04 22:48:35 -0700

    Add Favorite Container

---
+++ src/containers/FavoriteContainer.jsx
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import Favorite from '../components/Favorite';
+
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favorite);
