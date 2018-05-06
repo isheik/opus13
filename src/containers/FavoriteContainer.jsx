c410a65b4db6485b4a086a7a4d5fe05c1e0ab381
Keishi Asai <isheik808@gmail.com>
2018-05-05 23:59:39 -0700

    Add togglefav function

--- src/containers/FavoriteContainer.jsx
+++ src/containers/FavoriteContainer.jsx
@@ -7,11 +7,18 @@ const mapStateToProps = (state, props) => ({
   // favorited: props.favorited
 });
 
-const mapDispatchToProps = dispatch => (
+const mapDispatchToProps = (dispatch, props) => (
   {
     toggleFavorited: (favorited) => {
+      const twitterClient = new Twitter({
+        consumer_key: Authentication.APP_KEY,
+        consumer_secret: Authentication.APP_SECRET_KEY,
+        access_token_key: props.account.oauth_token,
+        access_token_secret: props.account.oauth_token_secret,
+      });
+
       favorited
-        ?
+        ? twitterClient()
         :;
 
     }
