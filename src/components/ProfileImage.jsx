570de5a8704bce83d49ed218a28920a87a056e8a
Keishi Asai <isheik808@gmail.com>
2018-05-06 17:09:32 -0700

    Add ProfileImage component

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProfileImage = props => (
+  <div>
+    <img src={props.profileImg} alt="" />
+  </div>
+);
+
+export default ProfileImage;
--- src/components/TweetItem.jsx
+++ src/components/TweetItem.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import FavoriteContainer from '../containers/FavoriteContainer';
+import ProfileImage from '../components/ProfileImage';
 
 const TweetItem = props => (
   <li>
-    <img src={props.tweet.user.profile_image_url_https} alt="" />
+    <ProfileImage profileImg={props.tweet.user.profile_image_url_https} />
     {props.tweet.text}
     <FavoriteContainer account={props.accounts[0]} tweet={props.tweet} />
   </li>
 );
 
+// <img src={props.tweet.user.profile_image_url_https} alt="" />
 export default TweetItem;
