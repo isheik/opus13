84b263384ff0a26acf6085cca94f403eb75480c1
Keishi Asai <isheik808@gmail.com>
2018-05-07 23:37:18 -0700

    Updaate header name style to have margin

--- src/components/TweetHeader.jsx
+++ src/components/TweetHeader.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+// TODO: Fix time format
+// TODO: Put some margin between elements
 const TweetHeader = props => (
   <div>
-    <span>{props.userName}</span>
+    <span className="header-username">{props.userName}</span>
     <span>{props.tweetTime}</span>
   </div>
 );
