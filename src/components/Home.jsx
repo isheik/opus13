31c5a80832ea51b26b31640bcda06d7d7e6decc0
Keishi Asai <isheik808@gmail.com>
2018-05-09 21:56:38 -0700

    Add user.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 // import TweetList from './TweetList';
 import EditorContainer from '../containers/EditorContainer';
 import TweetListContainer from '../containers/TweetListContainer';
+import User from './User';
 
 const Home = props => (
   <div>
-    <EditorContainer {...props} account={props.accounts && props.accounts[props.activeAccountIndex]} />
+    <div className="left-div">
+      <User account={props.accounts && props.accounts[props.activeAccountIndex]} />
+    </div>
+    <div className="right-div">
+      <EditorContainer {...props} account={props.accounts && props.accounts[props.activeAccountIndex]} />
+    </div>
     <TweetListContainer {...props} account={props.accounts && props.accounts[props.activeAccountIndex]} tab="home" />
   </div>
 );
