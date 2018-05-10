4b6e431619a8bfa1b590c02d814eee2597bce710
Keishi Asai <isheik808@gmail.com>
2018-05-09 22:33:51 -0700

    Modify token architecture

--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -39,32 +39,48 @@ const mapDispatchToProps = dispatch => (
     },
     subscribeIpcEvent: () => {
       ipcRenderer.on('twitter-auth-finish', (event, token) => {
-        const accounts = FileManager.readProperty('.opus13');
-        console.log(accounts);
+        // const accounts = FileManager.readProperty('.opus13');
+
+        // FUTURE: Specify an account when implementing multi account feature
+        // const account = accounts;
+        const account = token;
+        // console.log(accounts);
+
+        const twitterClient = new Twitter({
+          consumer_key: Authentication.APP_KEY,
+          consumer_secret: Authentication.APP_SECRET_KEY,
+          access_token_key: account.oauth_token,
+          access_token_secret: account.oauth_token_secret,
+        });
+
+        const params = {
+          user_id: account.user_id,
+        };
+
+        twitterClient.get('users/show', params, (error, response) => {
+          account.profile_img = response.profile_image_url_https;
+
+          // dispatch(actions.addAccount(token));
+          dispatch(actions.addAccount(account));
+          dispatch(actions.changeActiveAccount(0));
+
+          twitterClient.get('statuses/home_timeline', (error, tweets, response) => {
+            if (!error) {
+              for (let tweet of tweets) {
+                dispatch(actions.addTweetToTab(account, 'home', tweet));
+              }
+            }
+          });
+        });
         // dispatch(accountActions.addAccount(token));
         // dispatch(accountActions.changeActiveAccount());
-        dispatch(actions.addAccount(token));
-        dispatch(actions.changeActiveAccount(0));
         // console.log(accounts.oauth_token);
         // console.log(token);
 
         // TODO: Fix here
-        const twitterClient = new Twitter({
-          consumer_key: Authentication.APP_KEY,
-          consumer_secret: Authentication.APP_SECRET_KEY,
-          access_token_key: accounts.oauth_token,
-          access_token_secret: accounts.oauth_token_secret,
-        });
-
         // const params = { screen_name: accounts.screen_name };
         // twitterClient.get('statuses/user_timeline', params, (error, tweets, response) => {
-        twitterClient.get('statuses/home_timeline', (error, tweets, response) => {
-          if (!error) {
-            for (let tweet of tweets) {
-              dispatch(actions.addTweetToTab(accounts, 'home', tweet));
-            }
-          }
-        });
+
       });
     },
     getTweets: (account) => {
