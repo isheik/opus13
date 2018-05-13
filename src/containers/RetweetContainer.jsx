54bc7123caa5159a66e61ae2b7c71716af9d1ea7
Keishi Asai <isheik808@gmail.com>
2018-05-13 15:25:16 -0700

    Add RetweetContainer

---
+++ src/containers/RetweetContainer.jsx
@@ -0,0 +1,39 @@
+import { connect } from 'react-redux';
+import Twitter from 'twitter';
+import Retweet from '../components/Retweet';
+import Authentication from '../utils/Authentication';
+import actions from '../actions';
+
+const mapStateToProps = (state, props) => ({
+  // favorited: props.favorited
+});
+
+const mapDispatchToProps = (dispatch, props) => (
+  {
+    toggleFavorited: (tweet) => {
+      const twitterClient = new Twitter({
+        consumer_key: Authentication.APP_KEY,
+        consumer_secret: Authentication.APP_SECRET_KEY,
+        access_token_key: props.account.oauth_token,
+        access_token_secret: props.account.oauth_token_secret,
+      });
+
+      const params = {
+        id: tweet.id_str,
+      };
+
+      if (tweet.favorited) {
+        twitterClient.post('favorites/destroy', params, (error, returnedTweet, response) => {
+          // TODO: need to add tab info
+          dispatch(actions.addTweetToTab(props.account, 'home', returnedTweet));
+        });
+      } else {
+        twitterClient.post('favorites/create', params, (error, returnedTweet, response) => {
+          dispatch(actions.addTweetToTab(props.account, 'home', returnedTweet));
+        });
+      }
+    },
+  }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Retweet);
