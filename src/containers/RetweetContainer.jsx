3fa2c132c5e6ab450810a1656007c04a27d60cf5
Keishi Asai <isheik808@gmail.com>
2018-05-13 16:39:58 -0700

    Need to fix retweet

--- src/containers/RetweetContainer.jsx
+++ src/containers/RetweetContainer.jsx
@@ -21,50 +21,44 @@ const mapDispatchToProps = (dispatch, props) => (
       const params = {
         id: tweet.id_str,
       };
-      console.log(tweet);
 
       if (tweet.retweeted) {
         twitterClient.post('statuses/unretweet', params, (error, returnedTweet, response) => {
           // TODO: need to add tab info
           // TODO: del unretweetec tweet
+          console.log('unret');
           console.log(returnedTweet);
-          dispatch(actions.addTweetToTab(props.account, 'home', returnedTweet));
+          // TODO: del unretweetec tweet
+          // dispatch(actions.addTweetToTab(props.account, 'home', returnedTweet));
         });
       } else {
+        // NEXT: fix here cant auth
         twitterClient.post('statuses/retweet', params, (error, returnedTweet, response) => {
           console.log(returnedTweet);
-          dispatch(actions.addTweetToTab(props.account, 'home', returnedTweet));
+          console.log(twitterClient);
+          // dispatch(actions.addTweetToTab(props.account, 'home', returnedTweet));
+          const paramsa = {
+            status: returnedTweet,
+          };
+          twitterClient.post('statuses/update', paramsa, (error, tweet, response) => {
+            if (!error) {
+              console.log(tweet);
+              dispatch(actions.addTweetToTab(props.account, 'home', tweet));
+              // twitterClient.get('statuses/home_timeline', (error, tweets, response) => {
+              //   if (!error) {
+              //     // console.log('test');
+              //     for (let tweet of tweets) {
+              //       dispatch(actions.addTweetToTab(props.account, 'home', tweet));
+              //     }
+              //     // dispatch(actions.addTweetToTab(tweets, 'home'));
+              //   }
+              // });
+            } else {
+              console.log(error);
+            }
+          });
         });
       }
-    },
-    postTweet: (tweetText) => {
-      const twitterClient = new Twitter({
-        consumer_key: Authentication.APP_KEY,
-        consumer_secret: Authentication.APP_SECRET_KEY,
-        access_token_key: props.account.oauth_token,
-        access_token_secret: props.account.oauth_token_secret,
-      });
-
-      const params = {
-        status: tweetText,
-      };
-      twitterClient.post('statuses/update', params, (error, tweet, response) => {
-        if (!error) {
-          console.log(tweet);
-          dispatch(actions.addTweetToTab(props.account, 'home', tweet));
-          // twitterClient.get('statuses/home_timeline', (error, tweets, response) => {
-          //   if (!error) {
-          //     // console.log('test');
-          //     for (let tweet of tweets) {
-          //       dispatch(actions.addTweetToTab(props.account, 'home', tweet));
-          //     }
-          //     // dispatch(actions.addTweetToTab(tweets, 'home'));
-          //   }
-          // });
-        } else {
-          console.log(error);
-        }
-      });
     },
   }
 );
