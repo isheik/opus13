40a0c4dcde88f103c87378e720ad7e2ece44fcf6
Keishi Asai <isheik808@gmail.com>
2018-05-13 16:29:18 -0700

    Update retweet

--- src/containers/RetweetContainer.jsx
+++ src/containers/RetweetContainer.jsx
@@ -21,12 +21,14 @@ const mapDispatchToProps = (dispatch, props) => (
       const params = {
         id: tweet.id_str,
       };
+      console.log(tweet);
 
       if (tweet.retweeted) {
         twitterClient.post('statuses/unretweet', params, (error, returnedTweet, response) => {
           // TODO: need to add tab info
           // TODO: del unretweetec tweet
-          // dispatch(actions.addTweetToTab(props.account, 'home', returnedTweet));
+          console.log(returnedTweet);
+          dispatch(actions.addTweetToTab(props.account, 'home', returnedTweet));
         });
       } else {
         twitterClient.post('statuses/retweet', params, (error, returnedTweet, response) => {
@@ -34,6 +36,35 @@ const mapDispatchToProps = (dispatch, props) => (
           dispatch(actions.addTweetToTab(props.account, 'home', returnedTweet));
         });
       }
+    },
+    postTweet: (tweetText) => {
+      const twitterClient = new Twitter({
+        consumer_key: Authentication.APP_KEY,
+        consumer_secret: Authentication.APP_SECRET_KEY,
+        access_token_key: props.account.oauth_token,
+        access_token_secret: props.account.oauth_token_secret,
+      });
+
+      const params = {
+        status: tweetText,
+      };
+      twitterClient.post('statuses/update', params, (error, tweet, response) => {
+        if (!error) {
+          console.log(tweet);
+          dispatch(actions.addTweetToTab(props.account, 'home', tweet));
+          // twitterClient.get('statuses/home_timeline', (error, tweets, response) => {
+          //   if (!error) {
+          //     // console.log('test');
+          //     for (let tweet of tweets) {
+          //       dispatch(actions.addTweetToTab(props.account, 'home', tweet));
+          //     }
+          //     // dispatch(actions.addTweetToTab(tweets, 'home'));
+          //   }
+          // });
+        } else {
+          console.log(error);
+        }
+      });
     },
   }
 );
