02613bf987de4a4f1c8a182ab5f5d4ff077ee6ad
Keishi Asai <isheik808@gmail.com>
2018-05-13 17:57:22 -0700

    Fix not to change style depending on retweeted status

--- src/components/Retweet.jsx
+++ src/components/Retweet.jsx
@@ -22,19 +22,16 @@ class Retweet extends React.Component {
   render() {
     return (
       <div>
-        {
-          this.props.tweet.retweeted
-            ? <FontAwesomeIcon icon="retweet" color="#859900" onClick={this.handleRetweetClick} />
-            : <FontAwesomeIcon icon="retweet" onClick={this.handleRetweetClick} />
-        }
+        <FontAwesomeIcon icon="retweet" onClick={this.handleRetweetClick} />
       </div>
     );
   }
 }
 
-// {this.props.tweet.retweeted
-// ? (<FontAwesomeIcon icon="retweet" color="#b58900" onClick={this.handleRetweetClick} />)
-// : (<FontAwesomeIcon icon="retweet" onClick={this.handleRetweetClick} />)
+// {
+//   this.props.tweet.retweeted
+//     ? <FontAwesomeIcon icon="retweet" color="#859900" onClick={this.handleRetweetClick} />
+//     : <FontAwesomeIcon icon="retweet" onClick={this.handleRetweetClick} />
 // }
 
 
