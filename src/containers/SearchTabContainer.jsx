b6a9935e506fdf3b09d6c00408270fd11489a817
Keishi Asai <isheik808@gmail.com>
2018-06-21 00:21:29 -0700

    Add search tab container

---
+++ src/containers/SearchTabContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import SearchTab from '../components/SearchTab';
+
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchTab);
