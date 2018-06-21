12f3a869b424ac29ff622ddc84241e1230d49237
Keishi Asai <isheik808@gmail.com>
2018-06-21 00:43:47 -0700

    Made input filed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const SearchBar = props => (
-  <div>
-    <input type="text" />
-  </div>
-);
-
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: 'a',
+    };
+    this.changeText = this.changeText.bind(this);
+  }
+  changeText = (event) => {
+    this.setState({ searchText: event.target.text });
+  }
+  render() {
+    return (
+      <form>
+        <input type="text" onChange={this.changeText} />
+      </form>
+    );
+  }
+}
 export default SearchBar;
