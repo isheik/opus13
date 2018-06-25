85b3bde45a8460772a2cf2c81599afee315eca5f
Keishi Asai <isheik808@gmail.com>
2018-06-24 21:59:02 -0700

    Add search function

--- src/components/SearchTab.jsx
+++ src/components/SearchTab.jsx
@@ -4,7 +4,7 @@ import SearchBarContainer from '../containers/SearchBarContainer';
 
 const SearchTab = props => (
   <div>
-    <SearchBarContainer {...props} />
+    <SearchBarContainer {...props} account={props.accounts && props.accounts[props.activeAccountIndex]} />
     <TweetListContainer {...props} account={props.accounts && props.accounts[props.activeAccountIndex]} tab="search" />
   </div>
 );
--- src/containers/SearchBarContainer.jsx
+++ src/containers/SearchBarContainer.jsx
@@ -1,15 +1,42 @@
 import { connect } from 'react-redux';
+import Twitter from 'twitter';
+import actions from '../actions';
 import SearchBar from '../components/SearchBar';
+import Authentication from '../utils/authentication';
 
 const mapStateToProps = (state, props) => ({
 
 });
 
-const mapDispatchToProps = props => (
+const mapDispatchToProps = (dispatch, props) => (
   {
-    searchTwitter: searchText => {
+    searchTwitter: (searchText) => {
       // do twitter things
       console.log(searchText);
+      const twitterClient = new Twitter({
+        consumer_key: Authentication.APP_KEY,
+        consumer_secret: Authentication.APP_SECRET_KEY,
+        access_token_key: props.account.oauth_token,
+        access_token_secret: props.account.oauth_token_secret,
+      });
+
+      const queryString = encodeURIComponent(searchText);
+      const params = {
+        q: queryString,
+      };
+
+      twitterClient.get('search/tweets', params, (error, response) => {
+        if (!error) {
+          console.log(response);
+          console.log(error);
+          for (let tweet of response.statuses) {
+            // JUNE 25 need to clear state before putting new tweets ;otherwise, tweets are mixed when new queries are issued
+            dispatch(actions.addTweetToTab(props.account, 'search', tweet));
+          }
+        } else {
+          console.log(error);
+        }
+      });
     },
   }
 );
