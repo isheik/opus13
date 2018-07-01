2f3dbd4dc1c40550692fe5b675017dc0adba9b97
Keishi Asai <isheik808@gmail.com>
2018-06-30 21:57:04 -0700

    Clean up some codes

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -181,47 +181,47 @@ class Authentication {
     twitterAuthWindow.loadURL(authURL);
   }
 
-  static generateSignature() {
-    // Generate Key
-    const signatureKey = `${encodeURI(this.APP_SECRET_KEY)}&${encodeURI(this.ACCESS_TOKEN_SECRET)}`;
-    let signatureData = `${this.REQUEST_METHOD}&${this.REQUEST_URL}`;
+  // static generateSignature() {
+  //   // Generate Key
+  //   const signatureKey = `${encodeURI(this.APP_SECRET_KEY)}&${encodeURI(this.ACCESS_TOKEN_SECRET)}`;
+  //   let signatureData = `${this.REQUEST_METHOD}&${this.REQUEST_URL}`;
 
-    // Generate Data
-    const params = [
-      { key: 'oauth_callback', value: this.CALLBACK_URL },
-      { key: 'oauth_consumer_key', value: this.APP_KEY },
-      { key: 'oauth_signature_method', value: 'HMAC-SHA1' },
-      { key: 'oauth_timestamp', value: Date.now() },
-      { key: 'oauth_nonce', value: this.generateOAuthNonce() },
-      { key: 'oauth_version', value: '1.0' },
-    ];
+  //   // Generate Data
+  //   const params = [
+  //     { key: 'oauth_callback', value: this.CALLBACK_URL },
+  //     { key: 'oauth_consumer_key', value: this.APP_KEY },
+  //     { key: 'oauth_signature_method', value: 'HMAC-SHA1' },
+  //     { key: 'oauth_timestamp', value: Date.now() },
+  //     { key: 'oauth_nonce', value: this.generateOAuthNonce() },
+  //     { key: 'oauth_version', value: '1.0' },
+  //   ];
 
-    params.sort((a, b) => (
-      a.key.localeCompare(b.key)
-    ));
+  //   params.sort((a, b) => (
+  //     a.key.localeCompare(b.key)
+  //   ));
 
-    for (let i = 0; i < params.length; i++) {
-      params[i].value = encodeURIComponent(params[i].value);
-      signatureData += `&${params[i].key}=${params[i].value}`;
-    }
+  //   for (let i = 0; i < params.length; i++) {
+  //     params[i].value = encodeURIComponent(params[i].value);
+  //     signatureData += `&${params[i].key}=${params[i].value}`;
+  //   }
 
-    const hash = HmacSHA1(signatureData, signatureKey);
-    const signature = Base64.stringify(hash);
+  //   const hash = HmacSHA1(signatureData, signatureKey);
+  //   const signature = Base64.stringify(hash);
 
-    // console.log(signature);
-  }
+  //   // console.log(signature);
+  // }
 
-  // Generate random string for Oauth 1.0 oauth_nonce
-  static generateOAuthNonce() {
-    // removed chars from the last result
-    const nonWordChar = /[=+-]/gi;
-    const base64Str = btoa(`${this.APP_KEY}: ${Date.now()} `);
+  // // Generate random string for Oauth 1.0 oauth_nonce
+  // static generateOAuthNonce() {
+  //   // removed chars from the last result
+  //   const nonWordChar = /[=+-]/gi;
+  //   const base64Str = btoa(`${this.APP_KEY}: ${Date.now()} `);
 
-    // remove non word chars from the base64 result
-    const wordOnlyStr = base64Str.replace(nonWordChar, '');
+  //   // remove non word chars from the base64 result
+  //   const wordOnlyStr = base64Str.replace(nonWordChar, '');
 
-    return wordOnlyStr;
-  }
+  //   return wordOnlyStr;
+  // }
 }
 
 export default Authentication;
