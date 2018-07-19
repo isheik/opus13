d10b74d1ecacee5d9b5217cc417d6cfc1dbbcde2
Keishi Asai <isheik808@gmail.com>
2018-07-18 23:46:14 -0700

    Fix time format

--- src/components/TweetHeader.jsx
+++ src/components/TweetHeader.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
 // TODO: Fix time format do this next July 18
 // TODO: Put some margin between elements
 const TweetHeader = props => (
   <div>
     <span className="header-username">{props.userName}</span>
-    <span className="header-time">{props.tweetTime}</span>
+    <span className="header-time">{moment(props.tweetTime).format('h:mm:ss a - DD MMM YYYY')}</span>
   </div>
 );
 
