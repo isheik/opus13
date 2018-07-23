c992002da8ee6c807e77c85b44b8a36c60f81bcb
Keishi Asai <isheik808@gmail.com>
2018-07-22 23:27:55 -0700

    Fix import statement

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import Transition from 'react-transition-group';
+import { Transition } from 'react-transition-group';
 
 // const Favorite = props => (
 //   <div>
@@ -12,7 +12,6 @@ import Transition from 'react-transition-group';
 //     }
 //   </div>
 // );
-
 
 class Favorite extends React.Component {
   constructor() {
@@ -36,37 +35,72 @@ class Favorite extends React.Component {
   render() {
     console.log(this.state);
     return (
-      <div className="tweet-item-icon">
-        {
-          // tomorrow here
-          // <Transition in={this.props.tweet.favorited} timeout={500}>
-          // {state =>
-          // : (<FontAwesomeIcon icon={['far', 'star']} onClick={this.handleFavClick} />)
-          // }
-          // </Transition>
-          // <FontAwesomeIcon icon={this.state.favorited ? 'star' : ['far', 'star']} color={this.state.hovered ? "#b58900" : ""} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} onClick={this.handleFavClick} />
-        }
-
-        {
-          this.props.tweet.favorited
-            ? <FontAwesomeIcon
-              icon="star"
-              color="#b58900"
-              onClick={this.handleFavClick}
-            />
-            : <FontAwesomeIcon
-              icon={['far', 'star']}
-              color={this.state.hovered ? '#b58900' : ''}
-              onMouseEnter={this.onMouseEnter}
-              onMouseLeave={this.onMouseLeave}
-              onClick={this.handleFavClick}
-            />
-          // : (<FontAwesomeIcon icon={['far', 'star']} onClick={this.handleFavClick} />)
-        }
-      </div>
-    )
+      <Transition in={this.state.hovered} timeout={1000}>
+        <div className="tweet-item-icon">
+          {
+            this.props.tweet.favorited
+              ? <FontAwesomeIcon
+                icon="star"
+                color="#b58900"
+                onClick={this.handleFavClick}
+              />
+              : <FontAwesomeIcon
+                icon={['far', 'star']}
+                color={this.state.hovered ? '#b58900' : ''}
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+                onClick={this.handleFavClick}
+              />
+          }
+        </div>
+      </Transition >
+    );
   }
 }
+
+
+// class Favorite extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       hovered: false
+//     };
+//     this.onMouseEnter = this.onMouseEnter.bind(this);
+//     this.onMouseLeave = this.onMouseLeave.bind(this);
+//   }
+//   onMouseEnter() {
+//     this.setState({ hovered: true });
+//   }
+//   onMouseLeave() {
+//     this.setState({ hovered: false });
+//   }
+//   handleFavClick = () => {
+//     console.log('test');
+//     this.props.toggleFavorited(this.props.tweet);
+//   }
+//   render() {
+//     console.log(this.state);
+//     return (
+//       <div className="tweet-item-icon">
+//         {
+//           this.props.tweet.favorited
+//             ? <FontAwesomeIcon
+//               icon="star"
+//               color="#b58900"
+//               onClick={this.handleFavClick}
+//             />
+//             : <FontAwesomeIcon
+//               icon={['far', 'star']}
+//               color={this.state.hovered ? '#b58900' : ''}
+//               onMouseEnter={this.onMouseEnter}
+//               onMouseLeave={this.onMouseLeave}
+//               onClick={this.handleFavClick}
+//             />
+//         }
+//       </div>
+//     );
+//   }
+// }
 
 
 
