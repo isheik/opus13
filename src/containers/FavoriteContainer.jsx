58fde8e39e38256e2981f82e5c45882571b6928c
Keishi Asai <isheik808@gmail.com>
2018-07-29 15:36:53 -0700

    Fixed error

--- src/containers/FavoriteContainer.jsx
+++ src/containers/FavoriteContainer.jsx
@@ -31,7 +31,7 @@ const mapDispatchToProps = (dispatch, props) => (
       } else {
         const returnedTweet = await twitterClient.post('favorites/create', params);
         dispatch(actions.addTweetToTab(props.account, 'home', returnedTweet));
-        // dispatch(actions.addTweetToTab(props.account, 'favorite', returnedTweet));
+        dispatch(actions.addTweetToTab(props.account, 'favorite', returnedTweet));
       }
     },
   }
