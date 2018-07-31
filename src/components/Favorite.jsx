199af664bbfdcb0741f26630eab5d0e1143ff957
Keishi Asai <isheik808@gmail.com>
2018-07-30 23:39:12 -0700

    Fixed fav animation

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -35,84 +35,124 @@ class Favorite extends React.Component {
   render() {
     return (
       <div className="tweet-item-icon">
-        {
-          // July27 Fix here
-          this.props.tweet.favorited
-            ?
-            <Transition in={this.props.tweet.favorited} timeout={1000}>
-              {
-                (state) => {
-                  const styles = {
-                    entering: {
-                      // transform: 'rotate(45deg)',
-                      // transition: 'transform 0.2s linear',
-                      // fontSize: '50px',
-                      transform: 'scale(1.3) rotate(360deg)',
-                      transition: 'transform 0.1s ease-in',
-                    },
-                    entered: {
-                      // transform: 'rotate(45deg)',
-                      // transform: 'scale(1.2)',
-                      // transition: 'transform 0.1s linear',
-                      transform: 'scale(1.0) rotate(0deg)',
-                      transition: 'transform 0.1s ease-in',
-                      // fontSize: '50px',
-                    },
-                    exiting: {
-                      // transform: 'rotate(70deg)',
-                      transform: 'scale(1.3) rotate(360deg)',
-                      transition: 'transform 0.1s ease-in',
-                      // fontSize: '50px',
-                    },
-                    exited: {
-                      transform: 'scale(1.0)',
-                      // transform: 'rotate(0deg)',
-                      transition: 'transform 0.3s linear',
-                      // fontSize: '50px',
-                    },
-                  };
-                  console.log(state);
-                  console.log(this.props.tweet.favorited);
+        <Transition in={this.props.tweet.favorited} timeout={400}>
+          {
+            (state) => {
+              // July27 Fix here
 
-                  return (<FontAwesomeIcon
+              const styles = {
+                entering: {
+                  // transform: 'rotate(45deg)',
+                  // transition: 'transform 0.2s linear',
+                  // fontSize: '50px',
+                  transform: 'scale(1.2) rotate(30deg)',
+                  transition: 'transform 0.2s ease-in',
+                },
+                entered: {
+                  transform: 'scale(1.0) rotate(0deg)',
+                  transition: 'transform 0.2s ease-in',
+                },
+                exiting: {
+                  // transform: 'rotate(70deg)',
+                  // transform: 'scale(1.1) rotate(20deg)',
+                  // transition: 'transform 0.1s ease-in',
+                  // fontSize: '50px',
+                },
+                exited: {
+                  // transform: 'scale(1.0)',
+                  // transform: 'rotate(0deg)',
+                  // transition: 'transform 0.1s linear',
+                  // fontSize: '50px',
+                },
+              };
+              console.log(state);
+              console.log(this.props.tweet.favorited);
+
+              return (
+                this.props.tweet.favorited
+                  ?
+                  (<FontAwesomeIcon
                     icon="star"
                     color="#b58900"
                     style={styles[state]}
                     onClick={this.handleFavClick}
-                  />);
-                }
-              }
-            </Transition>
-            :
-            <FontAwesomeIcon
-              icon={['far', 'star']}
-              color={this.state.hovered ? '#b58900' : ''}
-              onMouseEnter={this.onMouseEnter}
-              onMouseLeave={this.onMouseLeave}
-              onClick={this.handleFavClick}
-            />
-        }
+                  />)
+                  :
+                  (<FontAwesomeIcon
+                    icon={['far', 'star']}
+                    color={this.state.hovered ? '#b58900' : ''}
+                    onMouseEnter={this.onMouseEnter}
+                    onMouseLeave={this.onMouseLeave}
+                    onClick={this.handleFavClick}
+                  />)
+              );
+            }
+          }
+        </Transition>
       </div>
     );
   }
 }
 
 
-// <Transition in={this.state.hovered} timeout={100}>
+// <div className="tweet-item-icon">
 //   {
-//     state => {
+//     // July27 Fix here
+//     this.props.tweet.favorited
+//       ?
+//       <Transition in={this.props.tweet.favorited} timeout={1000}>
+//         {
+//           (state) => {
+//             const styles = {
+//               entering: {
+//                 // transform: 'rotate(45deg)',
+//                 // transition: 'transform 0.2s linear',
+//                 // fontSize: '50px',
+//                 transform: 'scale(1.3) rotate(360deg)',
+//                 transition: 'transform 0.1s ease-in',
+//               },
+//               entered: {
+//                 // transform: 'rotate(45deg)',
+//                 // transform: 'scale(1.2)',
+//                 // transition: 'transform 0.1s linear',
+//                 transform: 'scale(1.0) rotate(0deg)',
+//                 transition: 'transform 0.1s ease-in',
+//                 // fontSize: '50px',
+//               },
+//               exiting: {
+//                 // transform: 'rotate(70deg)',
+//                 transform: 'scale(1.3) rotate(360deg)',
+//                 transition: 'transform 0.1s ease-in',
+//                 // fontSize: '50px',
+//               },
+//               exited: {
+//                 transform: 'scale(1.0)',
+//                 // transform: 'rotate(0deg)',
+//                 transition: 'transform 0.3s linear',
+//                 // fontSize: '50px',
+//               },
+//             };
+//             console.log(state);
+//             console.log(this.props.tweet.favorited);
 
-//       (<FontAwesomeIcon
+//             return (<FontAwesomeIcon
+//               icon="star"
+//               color="#b58900"
+//               style={styles[state]}
+//               onClick={this.handleFavClick}
+//             />);
+//           }
+//         }
+//       </Transition>
+//       :
+//       <FontAwesomeIcon
 //         icon={['far', 'star']}
-//         color={state === 'entered' ? '#b58900' : ''}
+//         color={this.state.hovered ? '#b58900' : ''}
 //         onMouseEnter={this.onMouseEnter}
 //         onMouseLeave={this.onMouseLeave}
 //         onClick={this.handleFavClick}
-//       />)
-//     }
+//       />
 //   }
-// </Transition >
-
-
+// </div>
 
 export default Favorite;
