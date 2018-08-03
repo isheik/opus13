a41a5975edf33c642c9778038feddd46a09e5530
Keishi Asai <isheik808@gmail.com>
2018-08-02 23:25:21 -0700

    Clean up comment

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -2,25 +2,12 @@ import React from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { Transition } from 'react-transition-group';
 
-// const Favorite = props => (
-//   <div>
-//     {
-//       console.log(props)}
-//     {props.favorited
-//       ? (<FontAwesomeIcon icon="star" color="yellow" />)
-//       : (<FontAwesomeIcon icon={['far', 'star']} />)
-//     }
-//   </div>
-// );
-
 class Favorite extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       hovered: false,
     };
-    // this.onMouseEnter = this.onMouseEnter.bind(this);
-    // this.onMouseLeave = this.onMouseLeave.bind(this);
   }
   onMouseEnter = () => {
     this.setState({ hovered: true });
@@ -93,66 +80,5 @@ class Favorite extends React.Component {
     );
   }
 }
-
-
-// <div className="tweet-item-icon">
-//   {
-//     // July27 Fix here
-//     this.props.tweet.favorited
-//       ?
-//       <Transition in={this.props.tweet.favorited} timeout={1000}>
-//         {
-//           (state) => {
-//             const styles = {
-//               entering: {
-//                 // transform: 'rotate(45deg)',
-//                 // transition: 'transform 0.2s linear',
-//                 // fontSize: '50px',
-//                 transform: 'scale(1.3) rotate(360deg)',
-//                 transition: 'transform 0.1s ease-in',
-//               },
-//               entered: {
-//                 // transform: 'rotate(45deg)',
-//                 // transform: 'scale(1.2)',
-//                 // transition: 'transform 0.1s linear',
-//                 transform: 'scale(1.0) rotate(0deg)',
-//                 transition: 'transform 0.1s ease-in',
-//                 // fontSize: '50px',
-//               },
-//               exiting: {
-//                 // transform: 'rotate(70deg)',
-//                 transform: 'scale(1.3) rotate(360deg)',
-//                 transition: 'transform 0.1s ease-in',
-//                 // fontSize: '50px',
-//               },
-//               exited: {
-//                 transform: 'scale(1.0)',
-//                 // transform: 'rotate(0deg)',
-//                 transition: 'transform 0.3s linear',
-//                 // fontSize: '50px',
-//               },
-//             };
-//             console.log(state);
-//             console.log(this.props.tweet.favorited);
-
-//             return (<FontAwesomeIcon
-//               icon="star"
-//               color="#b58900"
-//               style={styles[state]}
-//               onClick={this.handleFavClick}
-//             />);
-//           }
-//         }
-//       </Transition>
-//       :
-//       <FontAwesomeIcon
-//         icon={['far', 'star']}
-//         color={this.state.hovered ? '#b58900' : ''}
-//         onMouseEnter={this.onMouseEnter}
-//         onMouseLeave={this.onMouseLeave}
-//         onClick={this.handleFavClick}
-//       />
-//   }
-// </div>
 
 export default Favorite;
